2caca5728c800845f4ef0f82f7f595205daa6195
Khayrullokh <xayrullo0127@gmail.com>
2024-07-28 16:59:05 +0500

    Initial commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,11 @@ import TodoTextInput from "./todo-text-input";
 
 const Header = ({ onNewItem }: { onNewItem: (text: string) => void }) => (
   <header className="header">
-    <h1>todos</h1>
+    <h1>BookTick</h1>
+    <h2>There you can put books you want to read. After reading them you can delete and add new ones to the list</h2>
     <TodoTextInput
       initial=""
-      placeholder="What needs to be done?"
+      placeholder="Which books to read?"
       onSubmit={onNewItem}
     />
   </header>
